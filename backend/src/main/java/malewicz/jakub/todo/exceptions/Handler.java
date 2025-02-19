b4a7aaf65e57f6b1b88e638c39bb63371b17c037
Jakub Malewicz <kubamalewicz002@gmail.com>
2025-02-19 08:45:29 +0100

    feat: count performed requests

--- backend/src/main/java/malewicz/jakub/todo/exceptions/Handler.java
+++ backend/src/main/java/malewicz/jakub/todo/exceptions/Handler.java
@@ -5,13 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class Handler {
-
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
@@ -25,6 +25,12 @@ public class Handler {
         });
 
         return ErrorResponse.create(ex, HttpStatus.BAD_REQUEST, errorMessage.toString().trim());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ErrorResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException ex) {
+        return ErrorResponse.create(ex, HttpStatus.BAD_REQUEST, ex.getParameterName());
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
