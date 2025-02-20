395e2f6b7512658ce4e414f50ceb485c47920267
jakubmalewicz <kubamalewicz002@gmail.com>
2025-02-20 12:03:30 +0100

    build: dockerize frontend

--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -13,22 +13,22 @@ export class TaskService {
   }
 
   getTasks(date: Date): Observable<Task[]> {
-    return this.http.get<Task[]>("http://localhost:8080/api/v1/tasks", {params: {date: date.toLocaleDateString('en-CA')}})
+    return this.http.get<Task[]>(`${environment.apiUrl}/api/v1/tasks`, {params: {date: date.toLocaleDateString('en-CA')}})
   }
 
   deleteTask(id: string): Observable<void> {
-    return this.http.delete<void>("http://localhost:8080/api/v1/tasks/" + id);
+    return this.http.delete<void>(`${environment.apiUrl}/api/v1/tasks` + id);
   }
 
   markTaskAsCompleted(id: string): Observable<void> {
-    return this.http.patch<void>("http://localhost:8080/api/v1/tasks/" + id, {})
+    return this.http.patch<void>(`${environment.apiUrl}/api/v1/tasks` + id, {})
   }
 
   createTask(task:TaskDto): Observable<Task> {
-    return this.http.post<Task>("http://localhost:8080/api/v1/tasks", task)
+    return this.http.post<Task>(`${environment.apiUrl}/api/v1/tasks`, task)
   }
 
   updateTask(task:TaskDto, id: String): Observable<Task> {
-    return this.http.put<Task>("http://localhost:8080/api/v1/tasks/" + id, task)
+    return this.http.put<Task>(`${environment.apiUrl}/api/v1/tasks` + id, task)
   }
 }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://backend:8080',
 };
